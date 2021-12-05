96d92a6028292fb640ca8277a7b6ec59967110f3
Rif-7 <muhammadrifan974530@gmail.com>
2021-12-05 20:23:50 +0530

    add html-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "production",
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "The Restaurant",
+            template: "./src/template.html"
+        }),
+    ],
     module: {
         rules: [
             {
